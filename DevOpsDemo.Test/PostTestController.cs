55b9104cb73683bfeeea3d8a35c3fd1d3a332df3
A M <amster777@outlook.com>
2019-07-20 12:42:56 +0530

    Updated Test Cases

--- DevOpsDemo.Test/PostTestController.cs
+++ DevOpsDemo.Test/PostTestController.cs
@@ -19,7 +19,7 @@ namespace DevOpsDemo.Test
         public void Test_Index_View_Result()
         {
             //Arrange
-            var controller = new HomeController(this.repository);
+            var controller = new HomeController();
             //Act
             var result = controller.Index();
             //Assert
@@ -29,25 +29,11 @@ namespace DevOpsDemo.Test
         public void Test_Index_Return_Result()
         {
             //Arrange
-            var controller = new HomeController(this.repository);
+            var controller = new HomeController();
             //Act
             var result = controller.Index();
             //Assert
             Assert.NotNull(result);
-        }
-        [Fact]
-        public void Test_Index_GetPosts_MatchData()
-        {
-            //Arrange
-            var controller = new HomeController(this.repository);
-            //Act
-            var result = controller.Index();
-            //Assert
-            var viewResult = Assert.IsType<ViewResult>(result);
-            var model = Assert.IsAssignableFrom<List<PostViewModel>>(viewResult.ViewData.Model);
-            Assert.Equal(3, model.Count);
-            Assert.Equal(101, model[0].PostId);
-            Assert.Equal("DevOps Demo Title 1", model[0].Title);
-        }
+        } 
     }
 }
